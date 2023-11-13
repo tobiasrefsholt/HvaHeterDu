689acff027f458f58ad0929ecc13c0a160cd9112
Tobias Refsholt <tobias@refsholt.net>
2023-11-13 14:08:47 +0100

    Init

---
+++ HvaHeterDu/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace test
+{
+    internal class WhatsYourName
+    {
+
+        static void PrintToConsole()
+        {
+            Console.WriteLine("Denne metoden returnerer ingenting");
+        }
+        static void Main()
+        {
+            PrintToConsole();
+            Console.WriteLine("Hva heter du?");
+            string? inputName = Console.ReadLine();
+            Console.WriteLine("Hva gammel er du?");
+            string? inputAge = Console.ReadLine();
+            Console.WriteLine("Hvor bor du?");
+            string? inputWhere = Console.ReadLine();
+            Console.WriteLine($"Du heter {inputName}, er {inputAge} år gammel og kommer fra {inputWhere}.");
+        }
+    }
+}
+
